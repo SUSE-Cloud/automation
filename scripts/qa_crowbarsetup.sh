f2f29e54397fc093ea4a91272d70c3124e3ec11a
J. Daniel Schmidt <jdsn@suse.de>
2012-06-25 15:10:04 +0200

    several fixes for test cloud setup

--- scripts/qa_crowbarsetup.sh
+++ scripts/qa_crowbarsetup.sh
@@ -3,11 +3,13 @@
 test $(uname -m) = x86_64 || echo "ERROR: need 64bit"
 #resize2fs /dev/vda2
 
-cloud=d2
+cloud=${1:-d2}
+part1=${part1:-true}
+part2=${part2:-true}
 
 case $cloud in
 	d1)
-		net=178
+		net=10.122.178
 		net_storage=179
 		net_public=177
 		net_fixed=176
@@ -16,7 +18,7 @@ case $cloud in
 		vlan_fixed=566
 	;;
 	d2)
-		net=186
+		net=10.122.186
 		net_storage=187
 		net_public=185
 		net_fixed=184
@@ -25,7 +27,7 @@ case $cloud in
 		vlan_fixed=569
 	;;
 	p)
-		net=169
+		net=10.122.169
 		net_storage=170
 		net_public=168
 		net_fixed=160
@@ -33,24 +35,32 @@ case $cloud in
 		vlan_public=564
 		vlan_fixed=563
 	;;
+	virtual)
+		net=192.168.124
+	;;
 	*)
 		echo "Unknown Cloud"
 		exit 1
 	;;
 esac	
 
-echo configure static IP and absolute + resolvable hostname crowbar.$cloud.cloud.suse.de gw:10.122.$net.1
+if $part1 ; then
+
+echo configure static IP and absolute + resolvable hostname crowbar.$cloud.cloud.suse.de gw:$net.1
 cat > /etc/sysconfig/network/ifcfg-eth0 <<EOF
 NAME='eth0'
 STARTMODE='auto'
 BOOTPROTO='static'
-IPADDR='10.122.$net.10'
+IPADDR='$net.10'
 NETMASK='255.255.255.0'
-BROADCAST='10.122.$net.255'
+BROADCAST='$net.255'
 EOF
-echo "default 10.122.$net.1 - -" > /etc/sysconfig/network/routes
+ifdown br0
+rm -f /etc/sysconfig/network/ifcfg-br0
+echo "default $net.1 - -" > /etc/sysconfig/network/routes
 echo "crowbar.$cloud.cloud.suse.de" > /etc/HOSTNAME
 hostname `cat /etc/HOSTNAME`
+grep -q "$net.*crowbar" /etc/hosts || echo $net.10 crowbar.$cloud.cloud.suse.de crowbar >> /etc/hosts
 rcnetwork restart
 hostname -f # make sure it is a FQDN
 ping -c 1 `hostname -f`
@@ -65,10 +75,27 @@ mkdir -p /mnt/dist /mnt/cloud
 mkdir -p /srv/tftpboot/repos/Cloud/
 cd /srv/tftpboot/repos/Cloud/
 
-CLOUDDISTURL=dist.suse.de/ibs/Devel:/Cloud/images/iso
-CLOUDDISTURL=dist.suse.de/ibs/SUSE:/SLE-11-SP2:/Update:/Products:/Test/images/iso
-CLOUDDISTURL=dist.suse.de/install/SLE-11-SP2-CLOUD-Beta1
-wget -q -r -np -nc -A "SLE-CLOUD*Media1.iso" http://$CLOUDDISTURL/
+d=dist.suse.de
+case $cloudsource in
+	develcloud)
+		CLOUDDISTURL=$d/ibs/Devel:/Cloud/images/iso
+		CLOUDDISTISO="SLE-CLOUD*Media1.iso"
+	;;
+	susecloud)
+		CLOUDDISTURL=$d/ibs/SUSE:/SLE-11-SP2:/Update:/Products:/Test/images/iso
+		CLOUDDISTISO="SLE-CLOUD*Media1.iso"
+	;;
+	Beta*)
+		
+		CLOUDDISTURL=$d/install/SLE-11-SP2-CLOUD-$cloudsource/
+		CLOUDDISTISO="SLE-CLOUD*$cloudsource-DVD1.iso"
+	;;
+	*)
+		echo "Error: you must set environment variable cloudsource=develcloud|susecloud|Beta1"
+		exit 76
+	;;
+esac
+wget -q -r -np -nc -A "$CLOUDDISTISO" http://$CLOUDDISTURL/
 mount -o loop,ro $CLOUDDISTURL/*.iso /mnt/cloud
 rsync -a --delete-after /mnt/cloud/ . ; umount /mnt/cloud
 rm -rf "dist.suse.de"
@@ -85,7 +112,8 @@ if [ ! -e "/srv/tftpboot/suse-11.2/install/media.1/" ] ; then
 	rm *.iso
 fi
 
-sed -i.netbak -e "s/192.168.124/10.122.$net/g" \
+if [ $cloud != virtual ] ; then
+	sed -i.netbak -e "s/192.168.124/$net/g" \
               -e "s/192.168.125/10.122.$net_storage/g" \
               -e "s/192.168.123/10.122.$net_fixed/g" \
               -e "s/192.168.122/10.122.$net_public/g" \
@@ -93,28 +121,58 @@ sed -i.netbak -e "s/192.168.124/10.122.$net/g" \
               -e "s/300/$vlan_public/g" \
               -e "s/500/$vlan_fixed/g" \
               /opt/dell/barclamps/network/chef/data_bags/crowbar/bc-template-network.json
+fi
+if [ $cloud = p ] ; then
+	# production cloud has a /21 network
+	perl -i.perlbak -pe 'if(m/255.255.255.0/){$n++} if($n==3){s/255.255.255.0/255.255.248.0/}' /opt/dell/barclamps/network/chef/data_bags/crowbar/bc-template-network.json
+fi
 
 #+bmc router
 
 #fix autoyast xml.erb update channels
 if [ ! -e "/srv/tftpboot/repos/SLES11-SP2-Updates" ] ; then
-	sed -i.bak -e 's#<media_url>http://<%= @admin_node_ip %>:8091/repos/SLES11-SP\(.\)-Updates/</media_url>#<media_url>http://euklid.suse.de/mirror/SuSE/zypp-patches.suse.de/x86_64/update/SLE-SERVER/11-SP\1/</media_url>#' /opt/dell/barclamps/provisioner/chef/cookbooks/provisioner/templates/default/autoyast.xml.erb
+	sed -i.bak -e 's#<media_url>http://<%= @admin_node_ip %>:8091/repos/SLES11-SP\(.\)-Updates/</media_url>#<media_url>http://euklid.suse.de/mirror/SuSE/zypp-patches.suse.de/x86_64/update/SLE-SERVER/11-SP\1/</media_url>#'\
+    -e "s/<domain>[^<]*</<domain>$cloud.cloud.suse.de</" \
+    /opt/dell/barclamps/provisioner/chef/cookbooks/provisioner/templates/default/autoyast.xml.erb
 fi
 
+rm -f /tmp/chef-ready
 # run in screen to not lose session in the middle when network is reconfigured:
-screen -L /opt/dell/bin/install-chef-suse.sh
+screen -d -m -L /bin/bash -c '/opt/dell/bin/install-chef-suse.sh ; touch /tmp/chef-ready'
+n=300
+while [ $n -gt 0 ] && [ ! -e /tmp/chef-ready ] ; do
+	n=$(expr $n - 1)
+	sleep 1;
+	echo -n .
+done
+if [ $n = 0 ] ; then
+	echo "timed out waiting for chef-ready"
+	exit 83
+fi
+if ! curl -s http://localhost:3000 > /dev/null ; then
+	echo "crowbar self-test failed"
+	exit 84
+fi
+fi
+
+if $part2 ; then
 . /etc/profile.d/crowbar.sh
 
 #chef-client
-for i in 3 4 5 6 ; do
-  for pw in root crowbar ; do
-	  ipmitool -H "10.122.$net.16$i" -U root -P $pw lan set 1 defgw ipaddr "10.122.$net.1"
-	  ipmitool -H "10.122.$net.16$i" -U root -P $pw power reset
-  done
-done
+if [ $cloud != virtual ] ; then
+	for i in 3 4 5 6 ; do
+	  for pw in root crowbar ; do
+		  (ipmitool -H "$net.16$i" -U root -P $pw lan set 1 defgw ipaddr "$net.1"
+		  ipmitool -H "$net.16$i" -U root -P $pw power reset) &
+	  done
+	done
+	wait
+fi
 
 echo "Waiting for nodes to come up..."
-while ! crowbar machines list | grep ^d ; do sleep 1 ; done
+while ! crowbar machines list | grep ^d ; do sleep 10 ; done
+echo "Found one node"
+while test $(crowbar machines list | grep ^d|wc -l) -lt 2 ; do sleep 10 ; done
 echo "Sleeping 50 more seconds..."
 sleep 50
 for m in `crowbar machines list | grep ^d` ; do
@@ -126,6 +184,17 @@ for service in postgresql keystone glance nova nova_dashboard ; do
 	crowbar "$service" proposal commit default
 done
 
+fi
+
+
+
 #BMCs at 10.122.178.163-6 #node 6-9
 #BMCs at 10.122.$net.163-4 #node 11-12
 
+# undo propsal create+commit
+if false; then
+for service in nova_dashboard nova glance keystone postgresql ; do
+        crowbar "$service" proposal delete default
+        crowbar "$service" delete default
+done
+fi
