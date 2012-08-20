f60593cdb2ed9ffd469216b7fbf996f3dcb0cf93
Bernhard M. Wiedemann <bwiedemann@suse.de>
2012-08-20 15:08:30 +0200

    add test for reattaching a volume

--- scripts/qa_crowbarsetup.sh
+++ scripts/qa_crowbarsetup.sh
@@ -405,6 +405,9 @@ if [ -n "$testsetup" ] ; then
 		sleep 15
 		ssh $vmip fdisk -l /dev/vdb | grep 1073741824
 		volumeattachret=$?
+		nova volume-detach $instanceid 1
+		nova volume-attach $instanceid 1 /dev/vdb
+		ssh $vmip fdisk -l /dev/vdc | grep 1073741824 || volumeattachret=57
 		nova floating-ip-create | tee floating-ip-create.out
 		floatingip=$(perl -ne "if(/192\.168\.\d+\.\d+/){print \$&}" floating-ip-create.out)
 		nova add-floating-ip $instanceid $floatingip # insufficient permissions
