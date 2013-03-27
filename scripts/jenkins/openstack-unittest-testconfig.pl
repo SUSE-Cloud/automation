f6d25b3eb1fd591828d349ed1c25d10f02d8efa9
Dirk Mueller <dmueller@suse.com>
2013-03-27 12:35:49 +0100

    Add support for Grizzly:Staging

--- scripts/jenkins/openstack-unittest-testconfig.pl
+++ scripts/jenkins/openstack-unittest-testconfig.pl
@@ -46,6 +46,49 @@ my $uttrigger = {
         'python-oslo.config'          => "COMPONENT=python-oslo.config\n",
     }
   },
+  "Cloud:OpenStack:Grizzly:Staging" => {
+    functest   => { 'openstack-swift' => "COMPONENT=openstack-swift\n".
+                                         "SETUPCMD=remakerings &amp;&amp; swift-init main start\n".
+                                         "TESTCMD=./.functests\n".
+                                         "TEARDOWNCMD=swift-init main stop",
+                    'openstack-heat'  => "COMPONENT=openstack-heat\n".
+                                         "TESTCMD=./run_tests.sh -P -f"
+    },
+    probetests => { 'openstack-swift' => "COMPONENT=openstack-swift\n".
+                                         "SETUPCMD=remakerings &amp;&amp; swift-init main start\n".
+                                         "TESTCMD=./.probetests\n".
+                                         "TEARDOWNCMD=swift-init main stop"
+    },
+    unittest   => {
+        'openstack-ceilometer'        => "COMPONENT=openstack-ceilometer\n".
+                                         "SETUPCMD=rcmongodb start\n".
+                                         "TEARDOWNCMD=rcmongodb stop",
+        'openstack-cinder'            => "COMPONENT=openstack-cinder",
+        'openstack-dashboard'         => "COMPONENT=openstack-dashboard\n".
+                                         "TESTCMD=./run_tests.sh -N",
+        'openstack-glance'            => "COMPONENT=openstack-glance\n".
+                                         "TESTCMD=./run_tests.sh -N glance",
+        'openstack-heat'              => "COMPONENT=openstack-heat\n".
+                                         "TESTCMD=./run_tests.sh -P -u",
+        'openstack-keystone'          => "COMPONENT=openstack-keystone\n".
+                                         "TESTCMD=./run_tests.sh -N -P -xintegration",
+        'openstack-nova'              => "COMPONENT=openstack-nova\n" .
+                                         "TESTCMD=./run_tests.sh -N -P",
+        'openstack-quantum'           => "COMPONENT=openstack-quantum",
+        'openstack-swift'             => "COMPONENT=openstack-swift\n".
+                                         "SWIFT_TEST_CONFIG_FILE=/etc/swift/func_test.conf\n".
+                                         "TESTCMD=./.unittests",
+        'python-cinderclient'         => "COMPONENT=python-cinderclient",
+        'python-glanceclient'         => "COMPONENT=python-glanceclient\n",
+        'python-heatclient'           => "COMPONENT=python-heatclient\n",
+        'python-keystoneclient'       => "COMPONENT=python-keystoneclient",
+        'python-novaclient'           => "COMPONENT=python-novaclient",
+        'python-quantumclient'        => "COMPONENT=python-quantumclient\n",
+        'python-swiftclient'          => "COMPONENT=python-swiftclient\n",
+        'python-oslo.config'          => "COMPONENT=python-oslo.config\n",
+    }
+  },
+ 
   "Cloud:OpenStack:Folsom:Staging" => {
     functest   => { 'openstack-swift' => "COMPONENT=openstack-swift\n".
                                          "SETUPCMD=remakerings &amp;&amp; swift-init main start\n".
