5a3cf1872872a5dc03ee47f0fa27d35b074e0512
Adam Spiers <aspiers@suse.com>
2013-06-04 16:53:50 +0200

    streamline development / testing environment setup
    Add two new scripts which aim to ease creation and setup of
    a machine for Crowbar admin node installation.
    
    create-vm.sh creates fresh KVM VMs via libvirt.  This can be used to
    create both the Crowbar admin node VM and subsequent PXE-booting
    Crowbar nodes.
    
    crowbar-prep.sh prepares a host for Crowbar admin node installation.
    This takes care of making the required repositories available, and
    performs a few other tweaks.  The repositories can be provided via one
    of several mechanisms which aim to cover the most common scenarios,
    e.g. NFS-mounting from servers in Nuremberg, or from an NFS server on
    the VM host, or 9p virtio mounts from the VM host.
    
    These are mainly intended for use with the SUSE Cloud 2.0 kiwi
    appliance in the SUSE/cloud github repo:
    
      https://github.com/SUSE/cloud/tree/master/dev-setup/cloud-2.0/sles11-sp3

---
+++ scripts/create-vm.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+#
+# Creates a fresh KVM VM via libvirt.  This can be used to create both
+# the Crowbar admin node VM and subsequent PXE-booting Crowbar nodes.
+#
+# FIXME: ideally this would eventually be replaced by one or more
+# Vagrantfiles.
+
+usage () {
+    # Call as: usage [EXITCODE] [USAGE MESSAGE]
+    exit_code=1
+    if [[ "$1" == [0-9] ]]; then
+        exit_code="$1"
+        shift
+    fi
+    if [ -n "$1" ]; then
+        echo >&2 "$*"
+        echo
+    fi
+
+    me=`basename $0`
+
+    cat <<EOF >&2
+Usage: $me [options] VM-NAME VM-DISK VBRIDGE [FILESYSTEM-PATH]
+
+Options:
+  -h, --help     Show this help and exit
+EOF
+    exit "$exit_code"
+}
+
+parse_args () {
+    if [ "$1" == '-h' ] || [ "$1" == '--help' ]; then
+        usage 0
+    fi
+
+    if [ $# != 3 ]; then
+        usage
+    fi
+
+    vm_name="$1"
+    vm_disk="$2"
+    vbridge="$3"
+}
+
+main () {
+    # if [ `id -u` != 0 ]; then
+    #     echo "Please run as root." >&2
+    #     exit 1
+    # fi
+
+    parse_args "$@"
+
+    if ! /sbin/brctl show | egrep -q "^${vbridge}[[:space:]]"; then
+        usage "$vbridge is not a valid bridge device name"
+    fi
+
+    if [ -e "$vm_disk" ]; then
+        opts=(
+            --import
+            # virt-install doesn't support "readonly=true"
+            --filesystem /data/install,install
+        )
+    else
+        echo "Creating $vm_disk as qcow2 image ..."
+        qemu-img create -f qcow2 "$vm_disk" 4G
+        opts=( --pxe )
+    fi
+
+    # vm-install \
+    #     -n $vm_name \
+    #     -o sles11 \
+    #     -c4 \
+    #     -m2048 -M2048 \
+    #     -d qcow2:$vm_disk,xvda,disk,w,0,cachemode=none \
+    #     -e \
+    #     --nic bridge=$vbridge,model=virtio \
+    #     --keymap en-us
+
+    virt-install \
+        --connect qemu:///system \
+        --virt-type kvm \
+        --name $vm_name \
+        --ram 2048 \
+        --vcpus 4 \
+        ${opts[@]} \
+        --disk path=$vm_disk,format=qcow2,cache=none \
+        --os-type=linux \
+        --os-variant=sles11 \
+        --network bridge=$vbridge \
+        --graphics vnc
+}
+
+main "$@"
