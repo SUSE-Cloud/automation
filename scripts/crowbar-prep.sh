8b807095e7d5e8a6b13c7816c1386f3fcc573dcc
Adam Spiers <aspiers@suse.com>
2013-06-10 23:29:23 +0100

    crowbar-prep.sh: add nue-host-nfs profile
    This should be the best profile for most people in the Nuremberg office.

--- scripts/crowbar-prep.sh
+++ scripts/crowbar-prep.sh
@@ -34,6 +34,15 @@ usage () {
 Usage: cat $me | ssh root@ADMIN-NODE bash -s [OPTIONS] PROFILE
 
 Profiles:
+    nue-host-nfs
+        This profile is a hybrid of 'nue-nfs' and 'host-nfs' and is
+        probably the best trade-off between convenience and
+        flexibility if you are situated in the Nuremberg offices.  It
+        takes SLES repos from loki and sentosa, and the SUSE Cloud ISO
+        from the VM host ($HOST_IP) via NFS.  This allows you to
+        choose which SUSE Cloud build to develop/test against, but
+        eliminates the hassle of mirroring SLES repositories.
+
     nue-nfs
         Mount from {sentosa,loki}.suse.de.  This is a no-brainer
         setup which sacrifices flexibility for ease of setup.
@@ -206,14 +215,25 @@ bind_mount () {
     echo "$src $dst none bind,ro 0 0"
 }
 
+loki_nfs () {
+    loki=loki.suse.de:/vol/euklid/SuSE/zypp-patches.suse.de/x86_64/update/SLE-SERVER
+    nfs_mount $loki/11-SP3-POOL $POOL_MOUNTPOINT
+    nfs_mount $loki/11-SP3      $UPDATES_MOUNTPOINT
+}
+
+nue_host_nfs () {
+    (
+        nfs_mount sentosa.suse.de:/mnt/sles-11-sp3    $SP3_MOUNTPOINT
+        nfs_mount $HOST_IP:/mnt/suse-cloud-2.0        $CLOUD_MOUNTPOINT
+        loki_nfs
+    ) | append_to_fstab
+}
+
 nue_nfs () {
     (
         nfs_mount sentosa.suse.de:/mnt/sles-11-sp3    $SP3_MOUNTPOINT
         nfs_mount sentosa.suse.de:/mnt/suse-cloud-2.0 $CLOUD_MOUNTPOINT
-
-        loki=loki.suse.de:/vol/euklid/SuSE/zypp-patches.suse.de/x86_64/update/SLE-SERVER
-        nfs_mount $loki/11-SP3-POOL $POOL_MOUNTPOINT
-        nfs_mount $loki/11-SP3      $UPDATES_MOUNTPOINT
+        loki_nfs
     ) | append_to_fstab
 }
 
@@ -241,7 +261,7 @@ host_9p () {
 
 main () {
     case "$1" in
-        nue-nfs|host-nfs|host-9p)
+        nue-nfs|nue-host-nfs|host-nfs|host-9p)
             action="${1//-/_}"
             ;;
         *)
