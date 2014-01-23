33097d767ae9fef20a6fd34198dac0fa189de4e9
Dirk Mueller <dmueller@suse.com>
2014-01-23 12:29:14 +0100

    switch over to toci_devtest.sh
    The other scripts were deprecated.

--- scripts/jenkins/qa_tripleo.sh
+++ scripts/jenkins/qa_tripleo.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -eux
+
 # setup repos
 VERSION=11
 REPO=SLE_11_SP3
@@ -31,51 +33,40 @@ case "$cloudsource" in
   ;;
 esac
 
-# when using OSHEAD, dup from there
-if [ -n "$OSHEAD" ]; then
-    $zypper dup --from cloudhead
-    # use high prio so that packages will be preferred from here over Devel:Cloud
-    $zypper mr --priority 42 cloudhead
-fi
-if [ $VERSION = 11 ] ; then
-
-  if [ "$cloudsource" == "develcloud1.0" -o "$cloudsource" == "develcloud" ]; then
-      $zypper ar http://dist.suse.de/install/SLP/SLE-11-SP2-CLOUD-GM/x86_64/DVD1/ CloudProduct
-      $zypper ar http://download.nue.suse.com/ibs/SUSE:/SLE-11-SP2:/Update:/Products:/Test/standard/SUSE:SLE-11-SP2:Update:Products:Test.repo
-  else
-      $zypper rr CloudProduct || true
-      $zypper rr SUSE_SLE-11-SP2_Update_Products_Test || true
-  fi
-  if [ "$REPO" = SLE_11_SP2 ] ; then
-    $zypper ar http://$hostname/install/SLP/SLES-11-SP2-LATEST/$ARCH/DVD1/ SLES-11-SP2-LATEST
-    $zypper ar http://euklid.nue.suse.com/mirror/SuSE/zypp-patches.suse.de/$ARCH/update/SLE-SERVER/11-SP1/ SP1up # for python268
-    $zypper ar http://euklid.nue.suse.com/mirror/SuSE/zypp-patches.suse.de/$ARCH/update/SLE-SERVER/11-SP2/ SP2up
-    $zypper ar http://euklid.nue.suse.com/mirror/SuSE/zypp-patches.suse.de/$ARCH/update/SLE-SERVER/11-SP2-CORE/ SP2core
-  fi
-
-  if [ "$REPO" = SLE_11_SP3 ] ; then
-    $zypper ar 'http://smt-internal.opensuse.org/repo/$RCE/SLES11-SP3-Pool/sle-11-x86_64/' SP3Pool
-  fi
-fi
-
-
 $zypper -n --gpg-auto-import-keys ref
 $zypper in python-keystoneclient make patch python-PyYAML git-core busybox libvirt-client
-$zypper in libvirt-daemon-driver-network
+$zypper in libvirt-daemon-driver-network python-os-apply-config
 
 # Setup default.xml.. somehow we need this
-virsh net-define /usr/share/libvirt/networks/default.xml
+virsh net-define /usr/share/libvirt/networks/default.xml || :
 
 # Clean up from previous run
 rm -rf /tmp/toci*
 
-echo "{}" > /tmp/datafile
-export TE_DATAFILE=/tmp/datafile
-
 ## setup some useful defaults
-export TOCI_ARCH=x86_64
+export NODE_ARCH=amd64
+export TE_DATAFILE=~/tripleo/testenv.json
 
-export NODE_DIST="opensuse"
+export http_proxy=http://proxy.suse.de:3128/
+
+mkdir -p ~/tripleo/
+
+if [ ! -f ~/tripleo/testenv.json ]; then
+    ssh-keygen -f "private" -P ''
+    cat - > ~/tripleo/testenv.json <<EOF
+    {
+        "ssh-key": "$(base64 -w 0 < private)"
+    }
+EOF
+    rm -f private*
+fi
+
+if [ ! -d ~/tripleo/tripleo-incubator ]; then
+    (
+        cd ~/tripleo/
+        git clone git://git.openstack.org/openstack/tripleo-incubator
+    )
+fi
 
 if [ ! -d tripleo-ci ]; then
     git clone git://git.openstack.org/openstack-infra/tripleo-ci
@@ -83,11 +74,6 @@ fi
 
 cd tripleo-ci
 
-export http_proxy=http://proxy.suse.de:3128/
-
-# Apply seed-stack-config.json
-sed -i -e "s,\"i386\",\"$ARCH\"," /opt/toci/tripleo-image-elements/elements/seed-stack-config/config.json
-
-exec ./toci.sh
+exec ./toci_devtest.sh
 
 
