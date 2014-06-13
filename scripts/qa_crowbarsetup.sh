3c3723142250c08b214f9d4f3b4abdcfa08ccae0
Dirk Mueller <dmueller@suse.com>
2014-06-13 17:04:47 +0200

    Cleanup testvm so that it doesn't idle around
    That idling actually eats a bit of ressources.

--- scripts/qa_crowbarsetup.sh
+++ scripts/qa_crowbarsetup.sh
@@ -1187,6 +1187,7 @@ function do_testsetup()
 		nova volume-detach "$instanceid" "$volumeid" ; sleep 10
 		nova volume-attach "$instanceid" "$volumeid" /dev/vdb ; sleep 10
 		ssh $vmip fdisk -l /dev/vdb | grep 1073741824 || volumeattachret=57
+		nova delete testvm
 		test $volumecreateret = 0 -a $volumeattachret = 0
 	'
 	ret=$?
