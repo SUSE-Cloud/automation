SUSE-Cloud/automation

commit 120e99d3dd6e81592ca94b8a4c0cc9625c9933b4

crowbar-prep.sh: Invoke parse_opts before init_variables
This is necessary so that $CLOUD_VERSION is correctly propagated when
it's specified on the command line (otherwise it's always '3').

scripts/crowbar-prep.sh
@@ -609,8 +609,8 @@ parse_opts () {
 }
 
 main () {
-    init_variables
     parse_opts "$@"
+    init_variables
 
     case "$profile" in
         nue-nfs|nue-host-nfs|host-nfs|host-9p)

commit 668eb00cf438e5e922477da1961ae64810ce7385

fix usage text
120e99d3dd broke the usage text since default variables need to be set
prior to calling parse_opts() which calls usage().

scripts/crowbar-prep.sh
@@ -13,18 +13,21 @@
 me=`basename $0`
 [ "$me" = bash ] && me=crowbar-prep.sh
 
-# This is run prior to parsing options.
+# These have to be set prior to parse_opts() since usage()
+# depends on them being set.
+: ${CLOUD_VERSION_DEFAULT:=4}
+: ${HOST_MIRROR_DEFAULT:=/data/install/mirrors}
+: ${HOST_MEDIA_MIRROR_DEFAULT=/srv/nfs/media}
+
+# This is run after parse_opts(); see main() for details.
 init_variables () {
-    CLOUD_VERSION_DEFAULT=4
     : ${CLOUD_VERSION:=$CLOUD_VERSION_DEFAULT}
     set_cloud_version_variables
 
     : ${ADMIN_IP:=192.168.124.10}
     : ${HOST_IP:=192.168.124.1}
 
-    : ${HOST_MIRROR_DEFAULT=/data/install/mirrors}
     : ${HOST_MIRROR:=$HOST_MIRROR_DEFAULT}
-    : ${HOST_MEDIA_MIRROR_DEFAULT=/srv/nfs/media}
     : ${HOST_MEDIA_MIRROR:=$HOST_MEDIA_MIRROR_DEFAULT}
 
     # Product ISO filenames