SUSE-Cloud/automation

commit ba984dee8ff1281dd3d60705673f12e20d41207f

qa_crowbarsetup: cleanup floating IPs
this is to avoid resource leaks to be able to run the script regularly

scripts/qa_crowbarsetup.sh
@@ -1467,7 +1467,7 @@ EOH
         fi
         nova floating-ip-create | tee floating-ip-create.out
         floatingip=$(perl -ne "if(/\d+\.\d+\.\d+\.\d+/){print \$&}" floating-ip-create.out)
-        nova add-floating-ip "$instanceid" "$floatingip" # insufficient permissions
+        nova add-floating-ip "$instanceid" "$floatingip"
         vmip=$floatingip
         n=1000 ; while test $n -gt 0 && ! ping -q -c 1 -w 1 $vmip >/dev/null ; do
             n=$(expr $n - 1)
@@ -1525,7 +1525,10 @@ EOH
         nova volume-attach "$instanceid" "$volumeid" /dev/vdb ; sleep 10
         ssh $vmip fdisk -l /dev/vdb | grep 1073741824 || volumeattachret=57
         ssh $vmip "mount /dev/vdb /mnt && grep -q $rand /mnt/test.txt" || volumeattachret=58
-        nova stop testvm
+        # cleanup so that we can run testvm without leaking volumes, IPs etc
+        nova remove-floating-ip "$instanceid" "$floatingip"
+        nova floating-ip-delete "$floatingip"
+        nova stop "$instanceid"
 
         echo "Ceph Tests: $cephret"
         echo "RadosGW Tests: $radosgwret"

commit 7eedb81931e7a36482f03ba2461028bc3f61c68d

qa_crowbarsetup: split out addfloatingip function
this fixes reboot tests, which were broken by
commit ba984dee8ff1281dd3d60705673f12e20d41207f
    qa_crowbarsetup: cleanup floating IPs

scripts/qa_crowbarsetup.sh
@@ -1266,6 +1266,13 @@ function get_ceph_nodes()
     fi
 }
 
+function addfloatingip()
+{
+    local instanceid=$1
+    nova floating-ip-create | tee floating-ip-create.out
+    floatingip=$(perl -ne "if(/\d+\.\d+\.\d+\.\d+/){print \$&}" floating-ip-create.out)
+    nova add-floating-ip "$instanceid" "$floatingip"
+}
 
 function oncontroller_testsetup()
 {
@@ -1453,9 +1460,7 @@ EOH
             echo "Error: VM IP is empty. Exiting"
             exit 38
         fi
-        nova floating-ip-create | tee floating-ip-create.out
-        floatingip=$(perl -ne "if(/\d+\.\d+\.\d+\.\d+/){print \$&}" floating-ip-create.out)
-        nova add-floating-ip "$instanceid" "$floatingip"
+        addfloatingip "$instanceid"
         vmip=$floatingip
         n=1000 ; while test $n -gt 0 && ! ping -q -c 1 -w 1 $vmip >/dev/null ; do
             n=$(expr $n - 1)
@@ -1613,6 +1618,7 @@ function oncontroller_waitforinstance()
     nova list
     nova start testvm || exit 28
     nova list
+    addfloatingip testvm
     local vmip=`nova show testvm | perl -ne 'm/ fixed.network [ |]*[0-9.]+, ([0-9.]+)/ && print $1'`
     [[ -z "$vmip" ]] && complain 12 "no IP found for instance"
     wait_for 100 1 "ping -q -c 1 -w 1 $vmip >/dev/null" "testvm to boot up"