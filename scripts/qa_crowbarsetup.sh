65ed48617d069c824787deac469f657b3bb3f921
Michal Jura <mjura@suse.com>
2014-11-28 15:37:51 +0100

    Revert workaround for bnc#900112, which is fixed

--- scripts/qa_crowbarsetup.sh
+++ scripts/qa_crowbarsetup.sh
@@ -695,10 +695,6 @@ EOF
         /opt/dell/bin/json-edit -a attributes.network.networks.nova_floating.ranges.host.end -v $netp.167.191 $netfile
         # todo? broadcast
     fi
-    if [[ -n "$want_sles12" ]] ; then
-        # To avoid problems with: https://bugzilla.suse.com/show_bug.cgi?id=900112 ("wicked" issue)
-        /opt/dell/bin/json-edit -a attributes.network.enable_tx_offloading --raw -v "true" $netfile
-    fi
 
     cp -a $netfile /etc/crowbar/network.json # new place since 2013-07-18
 
