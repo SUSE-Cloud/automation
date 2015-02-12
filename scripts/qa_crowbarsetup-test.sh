629d24358bd651df8dd415466bb00cf75d8df7b6
Bernhard M. Wiedemann <bwiedemann@suse.de>
2015-02-12 16:40:57 +0100

    add test for iscloudver 4plus

--- scripts/qa_crowbarsetup-test.sh
+++ scripts/qa_crowbarsetup-test.sh
@@ -2,7 +2,7 @@
 
 describe "roundup(1) testing of qa_crowbarsetup.sh"
 
-verlist="3 4 5 6"
+verlist="3 4 5 6 4plus"
 
 cloudversionmatrixrow() {
     r=""
@@ -24,12 +24,12 @@ cloudversionmatrix() {
 
 it_returns_correct_cloudver_matrix() {
     results=`cloudversionmatrix "develcloud3 GM3 develcloud4 GM4 develcloud5 GM5"`
-    test "$results" = " develcloud3=0111 GM3=0111 develcloud4=1011 GM4=1011 develcloud5=1101 GM5=1101"
+    test "$results" = " develcloud3=01111 GM3=01111 develcloud4=10110 GM4=10110 develcloud5=11010 GM5=11010"
 }
 
 it_returns_correct_cloudver_matrix_milestone() {
     results=`cloudversionmatrix "M3 M4 M5 Beta3 Beta4 RC3 GMC susecloud5"`
-    test "$results" = " M3=1101 M4=1101 M5=1101 Beta3=1101 Beta4=1101 RC3=1101 GMC=1101 susecloud5=1101"
+    test "$results" = " M3=11010 M4=11010 M5=11010 Beta3=11010 Beta4=11010 RC3=11010 GMC=11010 susecloud5=11010"
 }
 
 getcloudversionmatrixrow() {
