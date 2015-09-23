6e740b0ab5d0dcc39aaaba8f60c53929091321b6
J. Daniel Schmidt <jdsn@suse.de>
2015-09-23 17:25:42 +0200

    qa_crowbarsetup: remove endless loop and drop allocation workaround

--- scripts/qa_crowbarsetup.sh
+++ scripts/qa_crowbarsetup.sh
@@ -1630,10 +1630,7 @@ function onadmin_allocate()
     echo "Allocating nodes..."
     local m
     for m in `get_all_discovered_nodes` ; do
-        while knife node show -a state $m | grep discovered; do # workaround bnc#773041
-            crowbar machines allocate "$m"
-            sleep 10
-        done
+        crowbar machines allocate $m
         local i=$(echo $m | sed "s/.*-0\?\([^-\.]*\)\..*/\1/g")
         cat >> .ssh/config <<EOF
 Host node$i
