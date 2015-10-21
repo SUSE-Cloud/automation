SUSE-Cloud/automation

commit 3e2d3341c1266960d6c2e8ddbc5347c05940653d

qa_crowbarsetup: When HA is used, deploy manila with HA

scripts/qa_crowbarsetup.sh
@@ -2180,6 +2180,11 @@ function custom_configuration()
                 proposal_set_value glance default "['deployment']['glance']['elements']['glance-server']" "['cluster:$clusternameservices']"
             fi
         ;;
+        manila)
+            if [[ $hacloud = 1 ]] ; then
+                proposal_set_value glance default "['deployment']['manila']['elements']['manila-server']" "['cluster:$clusternameservices']"
+            fi
+        ;;
         ceph)
             proposal_set_value ceph default "['attributes']['ceph']['disk_mode']" "'all'"
         ;;

commit 2d5677d13b56dd11964be9cfb1db1f4520019249

qa_crowbarsetup: Adjust correct proposal for Manila in HA
Alter manila and not glance proposal. Wrongly introduced by 3e2d3341c12669 .

scripts/qa_crowbarsetup.sh
@@ -2185,7 +2185,7 @@ function custom_configuration()
         ;;
         manila)
             if [[ $hacloud = 1 ]] ; then
-                proposal_set_value glance default "['deployment']['manila']['elements']['manila-server']" "['cluster:$clusternameservices']"
+                proposal_set_value manila default "['deployment']['manila']['elements']['manila-server']" "['cluster:$clusternameservices']"
             fi
         ;;
         ceph)