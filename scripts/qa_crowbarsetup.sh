SUSE-Cloud/automation

commit f98577e51c45954a718bb65e526981791d8c996b

Fix image upload for Liberty and newer
--copy-from was deprecated in Kilo and got removed in Liberty, so
we need to upload the image now differently

scripts/qa_crowbarsetup.sh
@@ -2887,15 +2887,25 @@ function oncontroller_testsetup()
     # Run Tempest Smoketests if configured to do so
     tempestret=0
     if [ "$wanttempest" = "1" ]; then
+        local image_name="SLE11SP3-x86_64-cfntools"
+
         # Upload a Heat-enabled image
-        openstack image list | grep -q SLE11SP3-x86_64-cfntools || openstack image create \
-            --public --disk-format qcow2 --container-format bare --property hypervisor_type=kvm \
-            --copy-from http://$clouddata/images/SLES11-SP3-x86_64-cfntools.qcow2 \
-            SLE11SP3-x86_64-cfntools | tee glance.out
+        if openstack image list | grep -q $image_name; then
+            openstack image show $image_name | tee glance.out
+        else
+            curl -s \
+                http://$clouddata/images/${image_name}.qcow2 | \
+                openstack image create \
+                    --public --disk-format qcow2 --container-format bare \
+                    --property hypervisor_type=kvm \
+                    $image_name | tee glance.out
+        fi
         imageid=`perl -ne "m/ id [ |]*([0-9a-f-]+)/ && print \\$1" glance.out`
         crudini --set /etc/tempest/tempest.conf orchestration image_ref $imageid
         # test if is cnftools image prepared for tempest
-        wait_for 300 5 'glance image-show $imageid | grep active &>/dev/null' "prepare cnftools image"
+        wait_for 300 5 \
+            'openstack image show $imageid | grep active &>/dev/null' \
+            "prepare cnftools image"
         pushd /var/lib/openstack-tempest-test
         echo 1 > /proc/sys/kernel/sysrq
         ./run_tempest.sh -N $tempestoptions 2>&1 | tee tempest.log

commit e6105ad7d6d37c2d26505122c3886d1fd83b70d5

qa_crowbarsetup: Fix name of heat image we're using
Got broken in f98577e5.

scripts/qa_crowbarsetup.sh
@@ -2878,7 +2878,7 @@ function oncontroller_testsetup()
     # Run Tempest Smoketests if configured to do so
     tempestret=0
     if [ "$wanttempest" = "1" ]; then
-        local image_name="SLE11SP3-x86_64-cfntools"
+        local image_name="SLES11-SP3-x86_64-cfntools"
 
         # Upload a Heat-enabled image
         if openstack image list | grep -q $image_name; then