e67b06bc4004cd0bee28e9536d682eac8c790df2
Bernhard M. Wiedemann <bwiedemann@suse.de>
2015-12-03 14:54:56 +0100

    qa_crowbarsetup: move ceilometer detection code
    to run it just once.
    Before this it ran for every proposal
    
    This is a refinement of c31b7ecc01799bdee5bd1d72cc7d754f0a2d17d6

--- scripts/qa_crowbarsetup.sh
+++ scripts/qa_crowbarsetup.sh
@@ -41,7 +41,6 @@ clusternamenetwork="network"
 wanthyperv=
 crowbar_api=http://localhost:3000
 crowbar_install_log=/var/log/crowbar/install.log
-ceilometerservice="ceilometer-cagent"
 
 export nodenumber=${nodenumber:-2}
 export tempestoptions=${tempestoptions:--t -s}
@@ -2232,9 +2231,6 @@ function custom_configuration()
     local proposaltype=${2:-default}
     local proposaltypemapped=$proposaltype
     proposaltype=${proposaltype%%+*}
-    if iscloudver 7plus || (iscloudver 6 && ! [[ $cloudsource =~ ^M[1-7]$ ]]); then
-        ceilometerservice="ceilometer-polling"
-    fi
 
     # prepare the proposal file to be edited, it will be read once at the end
     # So, ONLY edit the $pfile  -  DO NOT call "crowbar $x proposal .*" command
@@ -2392,6 +2388,10 @@ function custom_configuration()
             fi
         ;;
         ceilometer)
+            local ceilometerservice="ceilometer-cagent"
+            if iscloudver 7plus || (iscloudver 6 && ! [[ $cloudsource =~ ^M[1-7]$ ]]); then
+                ceilometerservice="ceilometer-polling"
+            fi
             if [[ $hacloud = 1 ]] ; then
                 proposal_set_value ceilometer default "['deployment']['ceilometer']['elements']['ceilometer-server']" "['cluster:$clusternameservices']"
                 proposal_set_value ceilometer default "['deployment']['ceilometer']['elements']['$ceilometerservice']" "['cluster:$clusternameservices']"
