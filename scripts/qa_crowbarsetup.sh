a6184257790c6cbc87d12abd96d1b9bf3b243075
Bernhard M. Wiedemann <bwiedemann@suse.de>
2016-02-26 14:04:10 +0100

    qa_crowbarsetup: fix cloud5 backup+restore
    we must only use the upgrade-backup codepath
    when moving to cloud6

--- scripts/qa_crowbarsetup.sh
+++ scripts/qa_crowbarsetup.sh
@@ -451,6 +451,12 @@ function iscloudver()
     return $?
 }
 
+function isupgradecloudver()
+{
+    local cloudsource=upgrade_cloudsource
+    iscloudver "$@"
+}
+
 function issusenode
 {
     local machine=$1
@@ -4002,7 +4008,7 @@ function onadmin_crowbarbackup()
         safely crowbarctl backup download $bid
         popd
         [[ -e /tmp/$btarball ]] || complain 12 "Backup tarball not created: /tmp/$btarball"
-    elif iscloudver 5 ; then
+    elif iscloudver 5 && isupgradecloudver 6 ; then
         # using the API, due to missing crowbarctl integration
         safely curl -s $crowbar_api_digest $crowbar_api/installer/upgrade/file > /tmp/$btarball
     else
