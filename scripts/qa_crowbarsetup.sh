ba9475ad2d44f53cb66d9b56cbe7108c8e166fd5
Bernhard M. Wiedemann <bwiedemann@suse.de>
2016-03-07 09:39:21 +0100

    qa_crowbarsetup: re-use d1.cloud for p1
    new production cloud needs nodes, IP ranges and VLANs

--- scripts/qa_crowbarsetup.sh
+++ scripts/qa_crowbarsetup.sh
@@ -167,7 +167,7 @@ setcloudnetvars()
     export cloudfqdn=${cloudfqdn:-$cloud.cloud.suse.de}
     if [ -z "$cloud" ] ; then
         complain 101 "Parameter missing that defines the cloud name" \
-            "Possible values: [d1, d2, p, virtual]" \
+            "Possible values: [p1, d2, p, virtual]" \
             "Example: $0 d2"
     fi
 
@@ -175,7 +175,7 @@ setcloudnetvars()
     netp=10.162
     net=${net_admin:-192.168.124}
     case "$cloud" in
-        d1)
+        p1)
             nodenumbertotal=5
             net=$netp.178
             net_public=$netp.177
