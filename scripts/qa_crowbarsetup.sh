8213c5f53b6c24ad811201a8dcbb1fdd89cfba16
Bernhard M. Wiedemann <bwiedemann@suse.de>
2016-03-10 09:19:45 +0100

    qa_crowbarsetup: allow to specify root password
    this makes it more suitable for production-level clouds

--- scripts/qa_crowbarsetup.sh
+++ scripts/qa_crowbarsetup.sh
@@ -23,6 +23,7 @@ fi
 : ${clouddata:=$(dig -t A +short clouddata.nue.suse.com)}
 : ${distsuse:=dist.nue.suse.com}
 distsuseip=$(dig -t A +short $distsuse)
+: ${want_rootpw:=linux}
 : ${want_raidtype:="raid1"}
 : ${want_multidnstest:=1}
 
@@ -2676,10 +2677,10 @@ function custom_configuration()
             fi
         ;;
         provisioner)
-            # set default password to 'linux'
-            proposal_set_value provisioner default "['attributes']['provisioner']['root_password_hash']" "\"$(openssl passwd -1 linux)\""
+            # set default password
+            proposal_set_value provisioner default "['attributes']['provisioner']['root_password_hash']" "\"$(openssl passwd -1 $want_rootpw)\""
             # set discovery root password too
-            iscloudver 6M12plus && proposal_set_value provisioner default "['attributes']['provisioner']['discovery']['append']" "\"DISCOVERY_ROOT_PASSWORD=linux\""
+            iscloudver 6M12plus && proposal_set_value provisioner default "['attributes']['provisioner']['discovery']['append']" "\"DISCOVERY_ROOT_PASSWORD=$want_rootpw\""
 
             if [[ $keep_existing_hostname = 1 ]] ; then
                 proposal_set_value provisioner default "['attributes']['provisioner']['keep_existing_hostname']" "true"
