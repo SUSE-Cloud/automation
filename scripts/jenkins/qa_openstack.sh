SUSE-Cloud/automation

commit 7861e41a0cb1dcba2903159f25a8eaca1370a562

qa_openstack: Also install openstackquickstartrc
Use /tmp/openstackquickstartrc when debugging qa_openstack.

scripts/jenkins/qa_openstack.sh
@@ -203,6 +203,9 @@ if [ -n "$QUICKSTART_DEBUG" ]; then
     test -e /tmp/openstack-loopback-lvm && \
         cp /tmp/openstack-loopback-lvm /usr/sbin/openstack-loopback-lvm && \
         echo "WARN: using /tmp/openstack-loopback-lvm"
+    test -e /tmp/rcopenstackquickstartrc && \
+        cp /tmp/rcopenstackquickstartrc /etc/rcopenstackquickstartrc && \
+        echo "WARN: using /tmp/rcopenstackquickstartrc"
 fi
 
 crudini=crudini

commit 9416b00bac602eb8393422c38d4ce16bd50aaf2f

qa_openstack: Fix openstackquickstartrc debugging
commit 7861e41a0cb1d introduced the wrong variable. The correct
name is "openstackquickstartrc".

scripts/jenkins/qa_openstack.sh
@@ -203,9 +203,9 @@ if [ -n "$QUICKSTART_DEBUG" ]; then
     test -e /tmp/openstack-loopback-lvm && \
         cp /tmp/openstack-loopback-lvm /usr/sbin/openstack-loopback-lvm && \
         echo "WARN: using /tmp/openstack-loopback-lvm"
-    test -e /tmp/rcopenstackquickstartrc && \
-        cp /tmp/rcopenstackquickstartrc /etc/rcopenstackquickstartrc && \
-        echo "WARN: using /tmp/rcopenstackquickstartrc"
+    test -e /tmp/openstackquickstartrc && \
+        cp /tmp/openstackquickstartrc /etc/openstackquickstartrc && \
+        echo "WARN: using /tmp/openstackquickstartrc"
 fi
 
 crudini=crudini