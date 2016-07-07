SUSE-Cloud/automation

commit 765b01256f7b74f67e3e75c2e5782ae83a8dbd05

Implement pci ordering for virtio disks (#1035)
* Code cleanup

* Cleanup pci dev specification

all pci parameters default to 0x00 so we only need to specify
the non-zero ones.

* Code cleanup

* Properly indent extra volume template

This makes it so much nicer to read in the
fixtures comparison

* Assign disks to PCI bus and give the predictable slots

This allows pci hotplug to be used on aarch64 as well
as creates a reliable and reproducible disk order in
case the VMs get restarted from template.

scripts/lib/libvirt/libvirt_setup.py
@@ -100,6 +100,10 @@ def net_config(args):
     return get_config(values, os.path.join(TEMPLATE_DIR, "admin-net.xml"))
 
 
+def merge_dicts(d1, d2):
+    return dict(it.chain(d1.items(), d2.items()))
+
+
 def compute_config(args, cpu_flags=cpuflags(), machine=None):
     if not machine:
         machine = get_default_machine()
@@ -108,14 +112,18 @@ def compute_config(args, cpu_flags=cpuflags(), machine=None):
     alldevices = it.chain(it.chain(string.lowercase[1:]),
                           it.product(string.lowercase, string.lowercase))
 
+    configopts = {
+        'nicmodel': 'e1000',
+        'emulator': args.emulator,
+        'vdisk_dir': args.vdiskdir,
+    }
     if hypervisor_has_virtio(libvirt_type):
-        nicmodel = "virtio"
         targetdevprefix = "vd"
-        targetbus = "virtio"
+        configopts['nicmodel'] = 'virtio'
+        configopts['target_bus'] = 'virtio'
     else:
-        nicmodel = "e1000"
         targetdevprefix = "sd"
-        targetbus = "ide"
+        configopts['target_bus'] = 'ide'
     controller_raid_volumes = args.controller_raid_volumes
     if args.nodecounter > args.numcontrollers:
         controller_raid_volumes = 0
@@ -127,9 +135,9 @@ def compute_config(args, cpu_flags=cpuflags(), machine=None):
     # a valid serial is defined in libvirt-1.2.18/src/qemu/qemu_command.c:
     serialcloud = re.sub("[^A-Za-z0-9-_]", "_", args.cloud)
     for i in range(1, controller_raid_volumes):
-        raid_template = string.Template(readfile(
-            "{0}/extra-volume.xml".format(TEMPLATE_DIR)))
-        raid_values = {
+        raid_template = string.Template(
+            readfile(os.path.join(TEMPLATE_DIR, "extra-volume.xml")))
+        raidvolume += "\n" + raid_template.substitute(merge_dicts({
             'volume_serial': "{0}-node{1}-raid{2}".format(
                 serialcloud,
                 args.nodecounter,
@@ -140,42 +148,41 @@ def compute_config(args, cpu_flags=cpuflags(), machine=None):
                 args.nodecounter,
                 i),
             'target_dev': targetdevprefix + ''.join(alldevices.next()),
-            'target_bus': targetbus
-        }
-        raidvolume += "\n" + raid_template.substitute(raid_values)
+            'target_slot': '0x2{0}'.format(i),
+        }, configopts))
 
     cephvolume = ""
     if args.cephvolumenumber and args.cephvolumenumber > 0:
         for i in range(1, args.cephvolumenumber+1):
-            ceph_template = string.Template(readfile(
-                "{0}/extra-volume.xml".format(TEMPLATE_DIR)))
-            ceph_values = dict(
-                volume_serial="{0}-node{1}-ceph{2}".format(
+            ceph_template = string.Template(
+                readfile(os.path.join(TEMPLATE_DIR, "extra-volume.xml")))
+            cephvolume += "\n" + ceph_template.substitute(merge_dicts({
+                'volume_serial': "{0}-node{1}-ceph{2}".format(
                     serialcloud,
                     args.nodecounter,
                     i),
-                source_dev="{0}/{1}.node{2}-ceph{3}".format(
+                'source_dev': "{0}/{1}.node{2}-ceph{3}".format(
                     args.vdiskdir,
                     args.cloud,
                     args.nodecounter,
                     i),
-                target_dev=targetdevprefix + ''.join(alldevices.next()),
-                target_bus=targetbus)
-            cephvolume += "\n" + ceph_template.substitute(ceph_values)
+                'target_dev': targetdevprefix + ''.join(alldevices.next()),
+                'target_slot': '0x1{0}'.format(i),
+            }, configopts))
 
     drbdvolume = ""
     if args.drbdserial:
-        drbd_template = string.Template(readfile(
-            "{0}/extra-volume.xml".format(TEMPLATE_DIR)))
-        drbd_values = dict(
-            volume_serial=args.drbdserial,
-            source_dev="{0}/{1}.node{2}-drbd".format(
+        drbd_template = string.Template(
+            readfile(os.path.join(TEMPLATE_DIR, "extra-volume.xml")))
+        drbdvolume = drbd_template.substitute(merge_dicts({
+            'volume_serial': args.drbdserial,
+            'source_dev': "{0}/{1}.node{2}-drbd".format(
                 args.vdiskdir,
                 args.cloud,
                 args.nodecounter),
-            target_dev=targetdevprefix + ''.join(alldevices.next()),
-            target_bus=targetbus)
-        drbdvolume = drbd_template.substitute(drbd_values)
+            'target_dev': targetdevprefix + ''.join(alldevices.next()),
+            'target_slot': '0x30'},
+            configopts))
 
     values = dict(
         cloud=args.cloud,
@@ -185,18 +192,15 @@ def compute_config(args, cpu_flags=cpuflags(), machine=None):
         march=get_machine_arch(),
         machine=machine,
         cpuflags=cpu_flags,
-        emulator=args.emulator,
-        vdisk_dir=args.vdiskdir,
         raidvolume=raidvolume,
         cephvolume=cephvolume,
         drbdvolume=drbdvolume,
         macaddress=args.macaddress,
-        nicmodel=nicmodel,
         target_dev=targetdevprefix + 'a',
-        target_bus=targetbus,
         bootorder=args.bootorder)
 
-    return get_config(values, os.path.join(TEMPLATE_DIR, "compute-node.xml"))
+    return get_config(merge_dicts(values, configopts),
+                      os.path.join(TEMPLATE_DIR, "compute-node.xml"))
 
 
 def domain_cleanup(dom):

scripts/lib/libvirt/fixtures/cloud-node1-raid.xml
@@ -26,30 +26,34 @@
       <driver name='qemu' type='raw' cache='unsafe'/>
       <source dev='/dev/cloud/cloud.node1'/>
       <target dev='vda' bus='virtio'/>
+      <address type='pci' slot='0x0a'/>
       <boot order='3'/>
     </disk>
 
-<disk type='block' device='disk'>
-  <serial>cloud-node1-raid1</serial>
-  <driver name='qemu' type='raw' cache='unsafe'/>
-  <source dev='/dev/cloud/cloud.node1-raid1'/>
-  <target dev='vdb' bus='virtio'/>
-</disk>
+    <disk type='block' device='disk'>
+      <serial>cloud-node1-raid1</serial>
+      <driver name='qemu' type='raw' cache='unsafe'/>
+      <source dev='/dev/cloud/cloud.node1-raid1'/>
+      <target dev='vdb' bus='virtio'/>
+      <address type='pci' slot='0x21'/>
+    </disk>
 
 
-<disk type='block' device='disk'>
-  <serial>cloud-node1-ceph1</serial>
-  <driver name='qemu' type='raw' cache='unsafe'/>
-  <source dev='/dev/cloud/cloud.node1-ceph1'/>
-  <target dev='vdc' bus='virtio'/>
-</disk>
+    <disk type='block' device='disk'>
+      <serial>cloud-node1-ceph1</serial>
+      <driver name='qemu' type='raw' cache='unsafe'/>
+      <source dev='/dev/cloud/cloud.node1-ceph1'/>
+      <target dev='vdc' bus='virtio'/>
+      <address type='pci' slot='0x11'/>
+    </disk>
 
-<disk type='block' device='disk'>
-  <serial>cloud-node1-ceph2</serial>
-  <driver name='qemu' type='raw' cache='unsafe'/>
-  <source dev='/dev/cloud/cloud.node1-ceph2'/>
-  <target dev='vdd' bus='virtio'/>
-</disk>
+    <disk type='block' device='disk'>
+      <serial>cloud-node1-ceph2</serial>
+      <driver name='qemu' type='raw' cache='unsafe'/>
+      <source dev='/dev/cloud/cloud.node1-ceph2'/>
+      <target dev='vdd' bus='virtio'/>
+      <address type='pci' slot='0x12'/>
+    </disk>
 
 
     <interface type='network'>
@@ -57,7 +61,7 @@
       <target dev='cloud-1'/>
       <source network='cloud-admin'/>
       <model type='virtio'/>
-      <address type='pci' domain='0x0000' bus='0x00' slot='0x03' function='0x0'/>
+      <address type='pci' slot='0x03'/>
       <boot order='2'/>
     </interface>
     <serial type='pty'>
@@ -70,7 +74,7 @@
     <graphics type='vnc' port='-1' autoport='yes'/>
     <video>
       <model type='cirrus' vram='9216' heads='1'/>
-      <address type='pci' domain='0x0000' bus='0x00' slot='0x02' function='0x0'/>
+      <address type='pci' slot='0x02'/>
     </video>
   </devices>
 </domain>

scripts/lib/libvirt/fixtures/cloud-node1.xml
@@ -26,16 +26,18 @@
       <driver name='qemu' type='raw' cache='unsafe'/>
       <source dev='/dev/cloud/cloud.node1'/>
       <target dev='vda' bus='virtio'/>
+      <address type='pci' slot='0x0a'/>
       <boot order='3'/>
     </disk>
 
 
-<disk type='block' device='disk'>
-  <serial>cloud-node1-ceph1</serial>
-  <driver name='qemu' type='raw' cache='unsafe'/>
-  <source dev='/dev/cloud/cloud.node1-ceph1'/>
-  <target dev='vdb' bus='virtio'/>
-</disk>
+    <disk type='block' device='disk'>
+      <serial>cloud-node1-ceph1</serial>
+      <driver name='qemu' type='raw' cache='unsafe'/>
+      <source dev='/dev/cloud/cloud.node1-ceph1'/>
+      <target dev='vdb' bus='virtio'/>
+      <address type='pci' slot='0x11'/>
+    </disk>
 
 
     <interface type='network'>
@@ -43,7 +45,7 @@
       <target dev='cloud-1'/>
       <source network='cloud-admin'/>
       <model type='virtio'/>
-      <address type='pci' domain='0x0000' bus='0x00' slot='0x03' function='0x0'/>
+      <address type='pci' slot='0x03'/>
       <boot order='2'/>
     </interface>
     <serial type='pty'>
@@ -56,7 +58,7 @@
     <graphics type='vnc' port='-1' autoport='yes'/>
     <video>
       <model type='cirrus' vram='9216' heads='1'/>
-      <address type='pci' domain='0x0000' bus='0x00' slot='0x02' function='0x0'/>
+      <address type='pci' slot='0x02'/>
     </video>
   </devices>
 </domain>

scripts/lib/libvirt/templates/compute-node.xml
@@ -22,6 +22,7 @@
       <driver name='qemu' type='raw' cache='unsafe'/>
       <source dev='$vdisk_dir/$cloud.node$nodecounter'/>
       <target dev='$target_dev' bus='$target_bus'/>
+      <address type='pci' slot='0x0a'/>
       <boot order='$bootorder'/>
     </disk>
 $raidvolume
@@ -32,7 +33,7 @@ $drbdvolume
       <target dev='$cloud-$nodecounter'/>
       <source network='$cloud-admin'/>
       <model type='$nicmodel'/>
-      <address type='pci' domain='0x0000' bus='0x00' slot='0x03' function='0x0'/>
+      <address type='pci' slot='0x03'/>
       <boot order='2'/>
     </interface>
     <serial type='pty'>
@@ -45,7 +46,7 @@ $drbdvolume
     <graphics type='vnc' port='-1' autoport='yes'/>
     <video>
       <model type='cirrus' vram='9216' heads='1'/>
-      <address type='pci' domain='0x0000' bus='0x00' slot='0x02' function='0x0'/>
+      <address type='pci' slot='0x02'/>
     </video>
   </devices>
 </domain>

scripts/lib/libvirt/templates/extra-volume.xml
@@ -1,6 +1,7 @@
-<disk type='block' device='disk'>
-  <serial>$volume_serial</serial>
-  <driver name='qemu' type='raw' cache='unsafe'/>
-  <source dev='$source_dev'/>
-  <target dev='$target_dev' bus='$target_bus'/>
-</disk>
+    <disk type='block' device='disk'>
+      <serial>$volume_serial</serial>
+      <driver name='qemu' type='raw' cache='unsafe'/>
+      <source dev='$source_dev'/>
+      <target dev='$target_dev' bus='$target_bus'/>
+      <address type='pci' slot='$target_slot'/>
+    </disk>

commit f4fe2c1c85e2f80d87d9fb1ba0f67629cf007604

libvirt_setup: fix xen testing (bsc#987584)
and hyperv, which broke in 765b01256f7b74f67e3e75c2e5782ae83a8dbd05
because ide disks dont allow address type pci

https://bugzilla.suse.com/show_bug.cgi?id=987584

scripts/lib/libvirt/libvirt_setup.py
@@ -219,8 +219,11 @@ def compute_config(args, cpu_flags=cpuflags(), machine=None):
         target_dev=targetdevprefix + 'a',
         bootorder=args.bootorder)
 
-    return get_config(merge_dicts(values, configopts),
-                      os.path.join(TEMPLATE_DIR, "compute-node.xml"))
+    config = get_config(merge_dicts(values, configopts),
+                        os.path.join(TEMPLATE_DIR, "compute-node.xml"))
+    if not hypervisor_has_virtio(libvirt_type):
+        config = re.sub(r"<address type='pci'[^>\n]*>", "", config)
+    return config
 
 
 def domain_cleanup(dom):