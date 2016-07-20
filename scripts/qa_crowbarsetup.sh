c209e28b50888ceb98de04477a24ac4a73b5c187
Michal Jura <mjura@suse.com>
2016-07-20 13:12:54 +0200

    Fix for depracated options

--- scripts/qa_crowbarsetup.sh
+++ scripts/qa_crowbarsetup.sh
@@ -3742,11 +3742,11 @@ function oncontroller_testsetup()
     nova flavor-delete m1.smaller || :
     nova flavor-create m1.smaller 11 512 8 1
     nova delete testvm  || :
-    nova keypair-add --pub_key /root/.ssh/id_rsa.pub testkey
+    nova keypair-add --pub-key /root/.ssh/id_rsa.pub testkey
     nova secgroup-add-rule default icmp -1 -1 0.0.0.0/0
     nova secgroup-add-rule default tcp 1 65535 0.0.0.0/0
     nova secgroup-add-rule default udp 1 65535 0.0.0.0/0
-    timeout 10m nova boot --poll --image $image_name --flavor $flavor --key_name testkey testvm | tee boot.out
+    timeout 10m nova boot --poll --image $image_name --flavor $flavor --nic net-name=fixed --key-name testkey testvm | tee boot.out
     ret=${PIPESTATUS[0]}
     [ $ret != 0 ] && complain 43 "nova boot failed"
     instanceid=`perl -ne "m/ id [ |]*([0-9a-f-]+)/ && print \\$1" boot.out`
