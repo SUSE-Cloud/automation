5e1d1ad87d73dd80b73d3b726c3b40f90bda7063
Johannes Grassler <johannes.grassler@suse.com>
2016-09-16 10:21:34 +0200

    Enabled Heat and Barbican in qa_devstack.sh.

--- scripts/jenkins/qa_devstack.sh
+++ scripts/jenkins/qa_devstack.sh
@@ -137,6 +137,9 @@ enable_service q-lbaas
 enable_plugin magnum https://git.openstack.org/openstack/magnum master
 # for testing
 #enable_service tempest
+ENABLED_SERVICES+=,heat,h-api,h-api-cfn,h-api-cw,h-eng
+IMAGE_URLS+=https://fedorapeople.org/groups/magnum/fedora-atomic-latest.qcow2
+enable_plugin barbican https://git.openstack.org/openstack/barbican
 EOF
 
     chown stack:stack -R $DEVSTACK_DIR
