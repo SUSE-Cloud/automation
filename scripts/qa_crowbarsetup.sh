af65099b50b9af708a6b6d903ec5049c8f9416fd
Bernhard M. Wiedemann <bwiedemann@suse.de>
2016-10-07 10:49:32 +0200

    qa_crowbarsetup: track time needed for steps
    to make it easier to compare timings of different runs

--- scripts/qa_crowbarsetup.sh
+++ scripts/qa_crowbarsetup.sh
@@ -5193,7 +5193,8 @@ function onadmin_teardown
 function onadmin_runlist
 {
     for cmd in "$@" ; do
-        onadmin_$cmd || complain $? "$cmd failed with code $?"
+        local TIMEFORMAT="timing for qa_crowbarsetup function 'onadmin_$cmd' real=%R user=%U system=%S"
+        time onadmin_$cmd || complain $? "$cmd failed with code $?"
     done
 }
 
