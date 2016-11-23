c4190d36974bbdce678019fb35118a37a476c0a7
J. Daniel Schmidt <jdsn@suse.de>
2016-11-23 15:56:00 +0100

    github-status: fix select statement
    'return' exits the method
    'next' exits the block

--- scripts/github-status/github-status.rb
+++ scripts/github-status/github-status.rb
@@ -77,7 +77,7 @@ class GHClientHandler
     pulls = get_all_pull_requests(state, status)
     pulls.select do |p|
       user = p.head.repo.owner.login rescue ''
-      return false unless user
+      next false unless user
       # team id 1541628 -> SUSE-Cloud/developers
       # team id 291046 -> crowbar/Owners
       # team id 159206 -> SUSE-Cloud/Owners
