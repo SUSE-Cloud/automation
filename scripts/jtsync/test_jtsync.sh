4aea420649d8eb6e339c397ede9587fd7ee6483d
Felix Schnizlein <felix@schnizle.in>
2017-01-17 16:00:14 +0100

    jtsync: Remove bundler calls from test. Add default test board to test

--- scripts/jtsync/test_jtsync.sh
+++ scripts/jtsync/test_jtsync.sh
@@ -1,55 +1,58 @@
 #!/usr/bin/env bash
 
+TEST_BOARD="fL7fv67z"
+RUBY=$(which ruby)
+
 echo "set (Cloud 7) cloud-mkcloud7-job-4nodes-linuxbridge-x86-64 to failed"
-bundle exec jtsync.rb --ci suse --job cloud-mkcloud7-job-4nodes-linuxbridge-x86-64 1
+${RUBY} jtsync.rb --board ${TEST_BOARD} --ci suse --job cloud-mkcloud7-job-4nodes-linuxbridge-x86-64 1
 echo "returncode: $?"
 read -p "Press any key to continue... " -n1 -s
 echo ""
 
 echo "set (Cloud 7) cloud-mkcloud7-job-4nodes-linuxbridge-x86-64 to failed (no notification)"
-bundle exec jtsync.rb --ci suse --job cloud-mkcloud7-job-4nodes-linuxbridge-x86-64 1
+${RUBY} jtsync.rb --board ${TEST_BOARD} --ci suse --job cloud-mkcloud7-job-4nodes-linuxbridge-x86-64 1
 echo "returncode: $?"
 read -p "Press any key to continue... " -n1 -s
 echo ""
 
 echo "set (Cloud 7) cloud-mkcloud7-job-4nodes-linuxbridge-x86-64 to success"
-bundle exec jtsync.rb --ci suse --job cloud-mkcloud7-job-4nodes-linuxbridge-x86-64 0
+${RUBY} jtsync.rb  --board ${TEST_BOARD} --ci suse --job cloud-mkcloud7-job-4nodes-linuxbridge-x86-64 0
 echo "returncode: $?"
 read -p "Press any key to continue... " -n1 -s
 echo ""
 
 echo "test: matrix job (cloud) to success"
-bundle exec jtsync.rb --ci suse --matrix crowbar-trackupstream,Devel:Cloud:7:Staging 0
+${RUBY} jtsync.rb --board ${TEST_BOARD} --ci suse --matrix crowbar-trackupstream,Devel:Cloud:7:Staging 0
 echo "returncode: $?"
 read -p "Press any key to continue... " -n1 -s
 echo ""
 
 echo "test: matrix job (cloud) to failed"
-bundle exec jtsync.rb --ci suse --matrix crowbar-trackupstream,Devel:Cloud:7:Staging 1
+${RUBY} jtsync.rb --board ${TEST_BOARD} --ci suse --matrix crowbar-trackupstream,Devel:Cloud:7:Staging 1
 echo "returncode: $?"
 read -p "Press any key to continue... " -n1 -s
 echo ""
 
 echo "test: matrix job (openstack)"
-bundle exec jtsync.rb --ci opensuse --matrix openstack-cleanvm,Juno 0
+${RUBY} jtsync.rb --board ${TEST_BOARD} --ci opensuse --matrix openstack-cleanvm,Juno 0
 echo "returncode: $?"
 read -p "Press any key to continue... " -n1 -s
 echo ""
 
 echo "test: search non existing job"
-bundle exec jtsync.rb --ci opensuse --matrix openstack-cleannothing,Juno 0
+${RUBY} jtsync.rb --board ${TEST_BOARD} --ci opensuse --matrix openstack-cleannothing,Juno 0
 echo "returncode: $?"
 read -p "Press any key to continue... " -n1 -s
 echo ""
 
 echo "test: invalid returncode"
-bundle exec jtsync.rb --ci opensuse --matrix openstack-cleanvm,Juno a
+${RUBY} jtsync.rb --board ${TEST_BOARD} --ci opensuse --matrix openstack-cleanvm,Juno a
 echo "returncode: $?"
 read -p "Press any key to continue... " -n1 -s
 echo ""
 
 echo "test: invalid ci type"
-bundle exec jtsync.rb --ci foobar --matrix openstack-cleanvm,Juno 0
+${RUBY} ./jtsync.rb --board ${TEST_BOARD}--ci foobar --matrix openstack-cleanvm,Juno 0
 echo "returncode: $?"
 read -p "Press any key to continue... " -n1 -s
 echo ""
