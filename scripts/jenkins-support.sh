18f79e2852b4a4c3b0e94885f452926d7b63751a
GOsipyan <Gayane.Osipyan@suse.com>
2017-02-22 13:58:48 +0100

    Add new job for running rally tasks on qa hw

---
+++ scripts/jenkins-support.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+function connect_rally_server_run_test
+{
+    # Connect to rally server and run tests
+    ssh -T root@$rally_server "bash -s $cloud $image_name" <<'EOF'
+cloud=$1
+image_name=$2
+
+# Check if specific cloud deployemnt already exist
+if ! (rally deployment list | grep -q $cloud); then
+    # Creat new cloud deployment env
+    rally deployment create --filename $cloud-deployment.json --name $cloud
+else
+    # Use existing deployment
+    rally deployment use $cloud
+fi
+source .openrc
+# Check if jeos image already exist on cloud
+# Upload new image in case no image found
+if ! openstack image show -c name --format value $image_name; then
+    wget -r -l1 --no-parent -A "SLES12-SP2-JeOS-for-OpenStack-Cloud.x86_64-1.2.0*.qcow2"  http://download.suse.de/ibs/SUSE:/SLE-12-SP2:/Update:/JeOS/images/ -O $image_name.qcow2
+    openstack image create --public --disk-format qcow2 --file $image_name.qcow2 $image_name
+    rm $image_name.qcow2
+fi
+
+# Run rally test
+rally task start rally/samples/tasks/scenarios/nova/nova.json
+
+# Generate run results output
+out_dir=/root/results
+rm -rf $out_dir
+mkdir -p /root/results
+rally task results > $out_dir/output.json
+rally task report $out_dir/output.json --out $out_dir/output.html
+EOF
+}
