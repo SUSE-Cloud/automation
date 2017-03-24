d9c17a12fd75536a2ef23bf59df5b375c49b2434
Bernhard M. Wiedemann <bwiedemann@suse.de>
2017-03-24 13:28:49 +0100

    production: split off import_images function
    and make it idempotent to make it easy to add and update images

--- scripts/qa_crowbarsetup.sh
+++ scripts/qa_crowbarsetup.sh
@@ -5430,12 +5430,16 @@ function oncontroller_setupproduction()
     openstack project create demo
     openstack user create --password demo --email ashish.sodhi@suse.com --project demo demo
     openstack role add --project demo --user demo Member
+    oncontroller_import_images
+}
 
-    # import images
+function oncontroller_import_images
+{
     for img in SLES12.qcow2 SLES12-SP1.qcow2 SLES12-SP2.qcow2 SLES12-SP3.qcow2 SP4-64up.qcow2 cirros-0.3.4-x86_64-disk.img openSUSE-Leap-42.1.qcow2 openSUSE-Leap-42.1-OpenStack.x86_64-0.0.3-Build1.2.qcow2 openSUSE-Leap-42.2-OpenStack.x86_64-0.0.4-Build3.1.qcow2 Fedora-x86_64-20-20131211.1-sda.qcow2 debian-jessie-nopart.qcow2 openSUSE-Leap-42.2-altimagebuild.qcow2 ; do
         local bimg=$(basename $img)
         local image_name=${bimg//.qcow2}
         local params="--disk-format qcow2 --container-format bare "
+        openstack image show $image_name > /dev/null && continue
         [[ $image_name =~ -Build ]] && params+=' --min-ram 300 --min-disk 10'
         curl -s \
             http://$clouddata/images/openstack/x86_64/$img | \
