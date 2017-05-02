390d4666175db26a21aedfb9d7bba7811ff1b656
J. Daniel Schmidt <jdsn@suse.de>
2017-05-02 12:49:46 +0200

    doc: drop obsolete variables from documentation

--- docs/basic-mkcloud-config.sh
+++ docs/basic-mkcloud-config.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-unset PARALLEL
 unset cloudpv
 unset cloudsource
 unset nodenumber
 unset want_sles12sp1
 
-export PARALLEL=yes
 export cloudpv=/dev/loop0
 export cloudsource=develcloud6
 export nodenumber='2'
