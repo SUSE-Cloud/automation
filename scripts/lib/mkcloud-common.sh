SUSE-Cloud/automation

commit be7dc5ec6a7c7a010a1f2cf2c35df77aae279e60

mkcloud-common: introduce variables for nfs, repo, rsync and smt server
The goal of this is to drop the $clouddata variable.
clouddata was a host at suse which offered many services in parallel.
We now need to support to configure the services individually and
therefore need separate variables.

The idea is:
 $reposerver, $nfsserver, $rsyncserver and $smtserver are only set from outside
They can be set to an ip or a hostname. But these variables must not
be used in the script(s).
The only ones that are to be used are the ones with the suffixes:
'*_ip' or '*_fqdn'

So this means:
The user sets
  export reposerver="somehostname.company.ldt"
mkcloud-common.sh will resolve to suffix variables.
In the code we only use:
  wget $reposerver_fqdn/some/path/package.rpm

scripts/lib/mkcloud-common.sh
@@ -549,7 +549,54 @@ function dist_to_image_name
 : ${distsuse:=dist.nue.suse.com}
 distsuseip=$(dig -t A +short $distsuse)
 : ${susedownload:=download.nue.suse.com}
-: ${smturl:="http://$susedownload/update/build.suse.de"}
+
+# NOTE: $clouddata and similar variables are deprecated
+if [[ $clouddata || $clouddatadns || $clouddata_base_path || $clouddata_nfs || $clouddata_nfs_dir ]] ; then
+    echo 'Warning: $clouddata and all related variables are deprecated.'
+    echo '  please use these new variables instead:'
+    echo '  - $reposerver'
+    echo '    - $reposerver_base_path'
+    echo '  - $nfsserver'
+    echo '    - $nfsserver_base_path'
+    echo '  - $smtserver'
+    : ${clouddatadns:=clouddata.nue.suse.com}
+    : ${clouddata:=$(dig -t A +short $clouddatadns)}
+    : ${clouddata_base_path:="/repos"}
+    : ${clouddata_nfs:=$clouddata}
+    : ${clouddata_nfs_dir:='srv/nfs'}
+    reposerver=$(dig -t A +short $clouddatadns)
+    reposerver_base_path=$clouddata_base_path
+    nfsserver=$(dig -t A +short $clouddatadns)
+    nfsserver_base_path=$clouddata_nfs_dir
+    unset clouddata clouddatadns clouddata_base_path clouddata_nfs clouddata_nfs_dir
+    sleep 5
+fi
+
+# $reposerver,$nfsserver,$rsyncserver,$smtserver are only set from outside
+# NOTE: they are not to be used in mkcloud/qa_crowbarsetup
+# Please ONLY use the suffixed variables: '*_ip' or '*_fqdn'
+
+: ${reposerver:=clouddata.nue.suse.com}
+: ${reposerver_ip:=$(to_ip $reposerver)}
+: ${reposerver_fqdn:=$(to_fqdn $reposerver)}
+: ${reposerver_base_path:=/repos}
+
+: ${nfsserver:=$reposerver}
+: ${nfsserver_ip:=$(to_ip $nfsserver)}
+: ${nfsserver_fqdn:=$(to_fqdn $nfsserver)}
+: ${nfsserver_base_path:=/srv/nfs}
+
+: ${rsyncserver:=$reposerver}
+: ${rsyncserver_ip:=$(to_ip $rsyncserver)}
+: ${rsyncserver_fqdn:=$(to_fqdn $rsyncserver)}
+: ${rsyncserver_images_dir:="cloud/images/$arch"}
+
+: ${smtserver:=$susedownload}
+: ${smtserver_ip:=$(to_ip $smtserver)}
+: ${smtserver_fqdn:=$(to_fqdn $smtserver)}
+: ${smturl:=http://$smtserver_fqdn/update/build.suse.de}
+
+
 if [[ $UID != 0 ]] ; then
     : ${sudo:=sudo}
     PATH=/sbin:/usr/sbin:/usr/local/sbin:$PATH

scripts/mkcloud
@@ -230,6 +230,24 @@ function sshrun
     cat > $mkcconf <<EOF
         export drbdnode_mac_vol=$drbdnode_mac_vol ;
         export cloud=$virtualcloud ;
+        # hostnames of repo, nfs, rsync and smt servers
+        # (*_ip and *_fqdn suffix variables are computed automatically)
+        export reposerver=$reposerver ;
+        export reposerver_ip=$reposerver_ip ;
+        export reposerver_fqdn=$reposerver_fqdn ;
+        export reposerver_base_path=$reposerver_base_path ;
+        export nfsserver=$nfsserver ;
+        export nfsserver_ip=$nfsserver_ip ;
+        export nfsserver_fqdn=$nfsserver_fqdn ;
+        export nfsserver_base_path=$nfsserver_base_path ;
+        export rsyncserver=$rsyncserver ;
+        export rsyncserver_ip=$rsyncserver_ip ;
+        export rsyncserver_fqdn=$rsyncserver_fqdn ;
+        export rsyncserver_images_dir=$rsyncserver_images_dir ;
+        export smtserver=$smtserver ;
+        export smtserver_ip=$smtserver_ip ;
+        export smtserver_fqdn=$smtserver_fqdn ;
+        export smturl=$smturl ;
         # hostname of NFS server with repos and images:
         export clouddata=$clouddata ;
         export clouddatadns=$clouddatadns ;

commit 36a2dff97bccefe9c93807e7493d626acdf01bf8

mkcloud: Add leading '/' in nfs_base_path
The `nfs_base_path` needs a leading `/`, otherwise the mount command
will fail. The code path with the incorrect path is executed when
`clouddata*` is set and was introduced in commit
be7dc5ec6a7c7a010a1f2cf2c35df77aae279e60 (which moves to drop the
`clouddata*` variables).

scripts/lib/mkcloud-common.sh
@@ -557,7 +557,7 @@ if [[ $clouddata || $clouddatadns || $clouddata_base_path || $clouddata_nfs || $
     : ${clouddata:=$(dig -t A +short $clouddatadns)}
     : ${clouddata_base_path:="/repos"}
     : ${clouddata_nfs:=$clouddata}
-    : ${clouddata_nfs_dir:='srv/nfs'}
+    : ${clouddata_nfs_dir:='/srv/nfs'}
     reposerver=$(dig -t A +short $clouddatadns)
     reposerver_base_path=$clouddata_base_path
     nfsserver=$(dig -t A +short $clouddatadns)