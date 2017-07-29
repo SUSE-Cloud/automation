dfdcb700a263cbf2100dbe8ca06f5a6d842b21b0
Bernhard M. Wiedemann <bwiedemann@suse.de>
2017-07-29 02:43:50 +0200

    p3: make patch non-interactive

--- hostscripts/productioncloud/setupproductioncloud.sh
+++ hostscripts/productioncloud/setupproductioncloud.sh
@@ -61,7 +61,7 @@ EOF
 )
 
 export pre_do_installcrowbar=$(base64 -w 0 <<EOF
-( cd /opt/dell/ ; ln -s barclamps/core/updates ; curl https://github.com/crowbar/crowbar-core/compare/stable/4.0...SUSE-Cloud:p2cloud.patch | patch -p1 )
+( cd /opt/dell/ ; ln -s barclamps/core/updates ; curl https://github.com/crowbar/crowbar-core/compare/stable/4.0...SUSE-Cloud:p2cloud.patch | patch -p1 --batch )
 EOF
 )
 
