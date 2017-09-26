08c504345d4038c734a4d61cdfc9c1088587ce17
Bernhard M. Wiedemann <bwiedemann@suse.de>
2017-09-26 15:28:38 +0200

    ecp1: move RAID config to ecp-specific place

--- scripts/qa_crowbarsetup.sh
+++ scripts/qa_crowbarsetup.sh
@@ -1603,6 +1603,9 @@ function onadmin_assign_hp_nodes
             DL360p)
                 if [[ controllern -lt 8 ]] ; then
                     set_node_alias_and_role $n controller$controllern controller
+                    if [[ $controller_raid_volumes -gt 1 ]] ; then
+                        set_node_raid $n $want_raidtype $controller_raid_volumes
+                    fi
                     let controllern++
                 else
                     set_node_alias_and_role $n compute$computen compute
@@ -1660,13 +1663,6 @@ function onadmin_allocate
         set_node_role_and_platform ${controllernodes[1]} "monitoring" $controller_os
     fi
 
-    # setup RAID for all controller nodes
-    if [[ $controller_raid_volumes -gt 1 ]] ; then
-        local controllernode
-        for controllernode in ${controllernodes[@]} ; do
-            set_node_raid $controllernode $want_raidtype $controller_raid_volumes
-        done
-    fi
 
     if [ -n "$want_node_os" ] ; then
         # OS for nodes provided explicitely: assign them successively to the nodes
