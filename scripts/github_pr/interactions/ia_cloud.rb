4ae372efde14781320059472c2ce32808aae4955
J. Daniel Schmidt <jdsn@suse.de>
2017-11-08 18:50:01 +0100

    github_pr: config for crowbar org

--- scripts/github_pr/interactions/ia_cloud.rb
+++ scripts/github_pr/interactions/ia_cloud.rb
@@ -29,4 +29,14 @@ module GithubPR
       para
     end
   end
+
+  class JenkinsJobTriggerCrowbarTestbuildAction < JenkinsJobTriggerAction
+    def extra_parameters(pull, _build_mode = "")
+      {
+        crowbar_repo: @metadata[:repository],
+        crowbar_github_pr: "#{pull.number}:#{pull.head.sha}:#{pull.base.ref}",
+        job_name: "#{@metadata[:org_repo]} testbuild PR #{pull.number} #{pull.head.sha[0,8]}",
+      }
+    end
+  end
 end
