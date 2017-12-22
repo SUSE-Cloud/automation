c65ae07835bce2429a3d1ca7aac3e215158e4018
Stefan Seyfried <stefan.seyfried@sap.com>
2017-12-22 15:59:01 +0100

    mkcloud-driver-libvirt: speed up qemu-img for file target
    If $admin_node_disk is *not* a block device (no LV, but a plain image
    file), then use caching and create a sparse file to hugely speed up the
    qemu-img conversion.

--- scripts/lib/mkcloud-driver-libvirt.sh
+++ scripts/lib/mkcloud-driver-libvirt.sh
@@ -374,8 +374,10 @@ function libvirt_do_onhost_deploy_image()
         fi
     fi
 
+    local qemu_img_extra=""
+    test -b $disk && qemu_img_extra="-t none -S 0" # sparse and cache only if it is not a blockdevice
     echo "Cloning $role node vdisk from $image ..."
-    safely $sudo qemu-img convert -t none -O raw -S 0 -p $image_path $disk
+    safely $sudo qemu-img convert -O raw $qemu_img_extra -p $image_path $disk
 
     if [[ ${resize_admin_node_partition:-1} = 1 ]]; then
         # resize the last partition only if it has id 83
