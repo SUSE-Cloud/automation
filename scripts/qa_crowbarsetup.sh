46d356e2192dd5f55fe8df394dba7b717956e04f
Bernhard M. Wiedemann <bwiedemann@suse.de>
2018-02-22 11:43:01 +0100

    production: add c8.large flavor

--- scripts/qa_crowbarsetup.sh
+++ scripts/qa_crowbarsetup.sh
@@ -5432,6 +5432,7 @@ function oncontroller_setupproduction()
     nova flavor-create --ephemeral 20 c8.small   33 2048 20 8
     nova flavor-create --ephemeral 20 c8.medium  34 4096 20 8
     nova flavor-create --ephemeral 20 c4.medium  35 4096 20 4
+    nova flavor-create                c8.large   50 8192 160 8
     nova flavor-create --ephemeral 80 d4.xlarge  40 16384 40 4
     nova flavor-create --ephemeral 20 m8.xlarge  41 32768 20 8
 
