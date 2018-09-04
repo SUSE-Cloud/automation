4b4555f60a8f1ab0daba0d6b2829013df8db747a
Dirk Mueller <dmueller@suse.com>
2018-09-04 11:02:17 +0200

    qa_crowbarsetup: wait longer for initial chef run
    On machines with low RAM (32GB in total) the initial chef
    run takes slightly longer, so we didn't wait enough half the time.
    Increasing the timeout fixes the issue.

--- scripts/qa_crowbarsetup.sh
+++ scripts/qa_crowbarsetup.sh
@@ -3966,7 +3966,7 @@ function oncontroller_testsetup
 
     local ssh_target="$ssh_user@$vmip"
 
-    wait_for 40 5 "timeout -k 20 10 ssh -o UserKnownHostsFile=/dev/null $ssh_target true" "SSH key to be copied to VM"
+    wait_for 60 5 "timeout -k 20 10 ssh -o UserKnownHostsFile=/dev/null $ssh_target true" "SSH key to be copied to VM"
 
     if ! ssh $ssh_target curl $test_internet_url ; then
         complain 95 could not reach internet
