cd40eea01e291c722179a2c2ecdaa3f33e3655f9
Joe Whitty <JWhitty@suse.com>
2019-02-13 09:09:50 -0800

    Moved resolution check above CustomFieldManager

--- jira/validators/worker.groovy
+++ jira/validators/worker.groovy
@@ -6,16 +6,16 @@ import com.opensymphony.workflow.WorkflowContext
 log.setLevel(Level.DEBUG)
 log.info("Start Worker Validation")
 
-// Check that we have a Worker
-def customFieldManager = ComponentAccessor.getCustomFieldManager()
-def workerCf = customFieldManager.getCustomFieldObjectByName("Worker")
-def worker = issue.getCustomFieldValue(workerCf)
-
 // If resolution is Rejected or Deferred return always true
 if ((issue.resolutionObject.name == "Rejected" || issue.resolutionObject.name == "Deferred")) {
     log.info("Status is Rejected or Deferred (true)")
     return true
 }
+
+// Check that we have a Worker
+def customFieldManager = ComponentAccessor.getCustomFieldManager()
+def workerCf = customFieldManager.getCustomFieldObjectByName("Worker")
+def worker = issue.getCustomFieldValue(workerCf)
 
 if (worker && worker != "unassigned") {
     log.info("Worker is assigned (true)")
