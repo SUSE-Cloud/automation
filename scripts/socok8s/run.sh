0279e07314ed1282ef185dc616107356e6ec38bc
Thomas Bechtold <tbechtold@suse.com>
2019-03-21 16:17:14 +0100

    github_pr: Add PR testing for socok8s
    We want to deploy a socok8s environment whenever a change is proposed
    to the suse-cloud/socok8s repo on github.
    Reuse what is already available to do that.

---
+++ scripts/socok8s/run.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -x
+
+# the jenkins job will pass these parameters to this script:
+#  <org> <repo> <pr_number> <sha1> 
+#
+
+org=$1
+repo=$2
+pr_id=$3
+sha1=$4
+
+
+pr_dir=$(mktemp -d cloud-socok8s.XXXXX)
+rsync -a $repo_dir/ "$pr_dir"
+pushd "$pr_dir"
+
+### copied from openstack-mkcloud job
+ghremote=origin
+git config --get-all remote.${ghremote}.fetch | grep -q pull || \
+    git config --add remote.${ghremote}.fetch "+refs/pull/*/head:refs/remotes/${ghremote}/pr/*"
+git fetch $ghremote 2>&1 | grep -v '\[new ref\]' || :
+git checkout -t $ghremote/pr/$pr_id
+git config user.email cloud-devel+jenkins@suse.de
+git config user.name "Jenkins User"
+echo "we merge to always test what will end up in master"
+git merge master -m temp-merge-commit
+# Show latest commit in log to see what's really tested.
+# Include a unique indent so that the log parser plugin
+# can ignore the output and avoid false positives.
+git --no-pager show | sed 's/^/|@| /'
+
+### run tox
+export PREFIX=socok8s-ci-${pr_id}-${sha1}
+echo "Prefix set to ${PREFIX}"
+./run.sh
+ret=$?
+
+# cleanup
+popd
+rm -rf "$pr_dir"
+exit "$ret"
