5c1bf782cb54887b9e421f1fe7c5343b72bc088d
Dirk Mueller <dmueller@suse.com>
2019-04-10 22:09:55 +0200

    mkcloud: add support for deploying octavia

--- scripts/lib/mkcloud-common.sh
+++ scripts/lib/mkcloud-common.sh
@@ -732,6 +732,7 @@ fi
 : ${want_designate_proposal:=0}
 : ${want_magnum_proposal:=0}
 : ${want_monasca_proposal:=0}
+: ${want_octavia_proposal:=0}
 : ${want_trove_proposal:=0}
 
 [ -z "$want_test_updates" -a -n "$TESTHEAD" ] && export want_test_updates=1
