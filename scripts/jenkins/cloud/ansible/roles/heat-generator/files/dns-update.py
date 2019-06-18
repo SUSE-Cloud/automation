109085a9f5f6eded6ea2afe1f6aabaf183980d7c
Nicolas Bock <nicolas.bock@suse.com>
2019-06-18 11:30:51 +0200

    Add option for name and path of cloudConfig.yml file
    This change adds a command line option to the dns script to specify
    the name and location of the `cloudConfig.yml` file.
    
    Signed-off-by: Nicolas Bock <nicolas.bock@suse.com>

--- scripts/jenkins/cloud/ansible/roles/heat-generator/files/dns-update.py
+++ scripts/jenkins/cloud/ansible/roles/heat-generator/files/dns-update.py
@@ -19,6 +19,11 @@ def parse_commandline():
         help="A list of ntp servers",
         nargs="+",
         default=[])
+    parser.add_argument(
+        "--cloud-config",
+        metavar="FILE",
+        help="The cloudConfig.yml FILE",
+        default="cloudConfig.yml")
     return parser.parse_args()
 
 
@@ -26,11 +31,11 @@ if __name__ == "__main__":
     options = parse_commandline()
     print(options)
 
-    with open('cloudConfig.yml') as f:
+    with open(options.cloud_config) as f:
         data = yaml.load(f.read(), Loader=yaml.SafeLoader)
 
     data['cloud']['dns-settings'] = dict(nameservers=options.dns_servers)
     data['cloud']['ntp-servers'] = options.ntp_servers
 
-    with open('cloudConfig.yml', 'w') as f:
+    with open(options.cloud_config, 'w') as f:
         f.write(yaml.safe_dump(data, default_flow_style=False))
