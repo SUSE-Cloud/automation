f8a3000f2f515ce821277545c7aba0cfbdfa0ce3
Stefan Nica <snica@suse.com>
2019-07-11 18:19:14 +0200

    qa_crowbarsetup.sh: setup magnum service in batch mode
    When deploying a crowbar batch scenario that includes magnum,
    the service image and flavors also need to set up.
    This is currently only done with the standard, non-batch way of
    deploying Crowbar.

--- scripts/qa_crowbarsetup.sh
+++ scripts/qa_crowbarsetup.sh
@@ -6002,6 +6002,10 @@ function onadmin_batch
                 ${scenario}
         safely crowbar batch --include manila --timeout 2400 build ${scenario}
     fi
+    if grep -q "barclamp: magnum" ${scenario}; then
+        get_novacontroller
+        safely oncontroller magnum_service_setup
+    fi
     return $?
 }
 
